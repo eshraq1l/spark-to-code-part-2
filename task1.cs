b0e7c2d91c1694e06dc661976a2df2330e24ce6e
Eshraq <eshraq416@gmail.com>
2026-07-04 22:38:35 +0400

    task 2

--- task1.cs
+++ task1.cs
@@ -2,7 +2,10 @@
 {
     internal class task1
     {
-       
+        /// <summary>
+        /// /////////////////////////////////////////
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.Write("Enter a starting number for the countdown: ");
@@ -17,3 +20,26 @@
         }
     }
 }
+///////////////////////////////////////////////////////
+///task2 
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum = sum + i;
+            }
+
+            Console.WriteLine("Sum from 1 to " + n + " = " + sum);
+        }
+    }
+}
+///////////////////////////////////////////////////////////////////////////
