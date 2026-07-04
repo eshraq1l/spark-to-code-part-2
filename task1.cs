6025cd5bb7c943084175fa16becc1f0b8828dd4c
Eshraq <eshraq416@gmail.com>
2026-07-04 22:30:07 +0400

    task 1

---
+++ task1.cs
@@ -0,0 +1,19 @@
+﻿namespace spark_to_code_part_2
+{
+    internal class task1
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a starting number for the countdown: ");
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = start; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Lift off!");
+        }
+    }
+}
